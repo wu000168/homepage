fe700bc68e6218505a16b7e2c25e4154ab4d0701
wu000168 <wu000168@users.noreply.github.com>
2020-08-04 04:18:28 +0000

    deploy: 11fc9774a06fb343a7237edc9ff6f12a4ef05fc8

---
+++ precache-manifest.d0b19f3f166f26ae2055fa9534008c4a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caf4fc1fcbab57d026a6a4a6ed5add1",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "616c15dd7fb3d986fd41",
+    "url": "/homepage/static/css/main.05207aa2.chunk.css"
+  },
+  {
+    "revision": "9daae4bab2516e739e25",
+    "url": "/homepage/static/js/2.97855561.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/homepage/static/js/2.97855561.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616c15dd7fb3d986fd41",
+    "url": "/homepage/static/js/main.2db5d4a1.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "cb59a1c67f6bd25df8c43201e0ad8543",
+    "url": "/homepage/static/media/photo.cb59a1c6.jpeg"
+  }
+]);
